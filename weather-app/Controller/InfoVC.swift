60bbb0516c4bd4b2d6f4516e2ffa4cdcf505383f
Daniil <kuningram@gmail.com>
2021-02-17 16:34:44 +0300

    added SearchBar

--- weather-app/Controller/InfoVC.swift
+++ weather-app/Controller/InfoVC.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class InfoVC: UIViewController {
 
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    var model: ResultModel! 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateContent()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateContent() {
+        self.cityLabel.text = model.name
     }
-    */
 
 }
--- weather-app/Controller/SearchVC.swift
+++ weather-app/Controller/SearchVC.swift
@@ -10,11 +10,111 @@ import UIKit
 class SearchVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
+    
+    var cities = [
+        // berlin
+        [52.52437, 13.41053],
+        // st petersburg
+        [59.9500, 30.3167],
+        // moscow
+        [55.751244, 37.618423],
+        // minsk
+        [53.9022, 27.5618],
+        // hamburg
+        [53.5500, 10.0000],
+        // munich
+        [48.1372, 11.5755],
+    ]
+    
+    var webService = WebService()
+    
+    var models : [ResultModel]? = nil
+    
+    var filteredModels : [ResultModel]!
+    
+    var needsRefresh = true
+    
+    var selectedIndex = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webService.delegate = self
+        fetch()
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
     }
     
+    func fetch() {
+        for city in cities {
+            webService.fetchWeather(lat: city[0], lon: city[1])
+        }
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showInfo" {
+            let destinationVC = segue.destination as! InfoVC
+            destinationVC.model = self.models![selectedIndex]
+        }
+    }
 
 }
+
+extension SearchVC: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.filteredModels = models!.filter {
+            $0.name!.lowercased().contains(searchText.lowercased())
+        }
+        self.tableView.reloadData()
+    }
+}
+
+
+extension SearchVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.selectedIndex = indexPath.row
+        self.performSegue(withIdentifier: "showInfo", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredModels?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! Cell
+        guard let model = filteredModels?[indexPath.row] else { return cell }
+        cell.model = model
+        return cell
+    }
+    
+}
+
+extension SearchVC: WebServiceDelegate {
+    
+    func didUpdate(_ apiManager: WebService, model: ResultModel) {
+        DispatchQueue.main.async {
+            if (self.needsRefresh) {
+                self.models = [ResultModel]()
+                self.models!.append(model)
+                self.needsRefresh = false
+            }
+            else {
+                self.models!.append(model)
+            }
+            self.filteredModels = self.models
+            
+            self.tableView.reloadData()
+            
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+
